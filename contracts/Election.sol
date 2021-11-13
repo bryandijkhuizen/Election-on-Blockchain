ab79ef89749e52be2ad60820726cf3a0074d4415
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-11-13 11:51:30 +0100

    Added a Smart Contract (Election)

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    //Store candidate
+    //Read candidate
+    //create a public candidate variable
+    string public candidate;
+    //create a constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+
+    
+}
